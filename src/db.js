cbd04ccfc3f034448d95512349f0e5b0362d1d34
MinPus <minhphucnguyen2k3@gmail.com>
2025-03-15 00:41:48 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,26 +1,20 @@
+// File: db.js
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10, // Số kết nối tối đa trong Pool
-    queueLimit: 0,
-    enableKeepAlive: true,  // Giữ kết nối luôn hoạt động
-    idleTimeout: 30000, // Đóng kết nối sau 30 giây nếu không dùng
-    maxIdle: 5 // Giữ tối đa 5 kết nối rảnh trong pool
+    database: process.env.DB_NAME
 });
 
-pool.getConnection((err, connection) => {
+db.connect((err) => {
     if (err) {
         console.error("Database connection failed: ", err);
         return;
     }
     console.log("Connected to MySQL database");
-    connection.release(); // Giải phóng kết nối sau khi kiểm tra
 });
 
-module.exports = pool.promise();
+module.exports = db;
